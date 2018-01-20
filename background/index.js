0c3eb133644a1946cf933ae9704f3e99f0e96eac
John Freeman <jfreeman08@gmail.com>
2018-01-20 00:44:58 -0500

    Listen for click on extension icon

--- background/index.js
+++ background/index.js
@@ -1,1 +1,5 @@
-console.log('Hello, world!')
+var console = chrome.extension.getBackgroundPage().console
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  console.log('Hello, world!')
+})
